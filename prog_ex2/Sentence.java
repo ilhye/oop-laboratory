0a91f7c8e80d9c7b7940c88972b846a59bc1cc7a
ilhye <144883820+ilhye@users.noreply.github.com>
2024-02-21 20:24:09 +0800

    fixed syntax

--- prog_ex2/Sentence.java
+++ prog_ex2/Sentence.java
@@ -2,48 +2,52 @@ import java.util.*; //for Scanner class
 
 public class Sentence {
     static Scanner input = new Scanner(System.in); // create object that will read input
+
     public static void main(String[] args) {
         System.out.print("Enter sentence: ");
         String strSentence = input.nextLine(); // get sentence
-        
+
         // replace all punctuations with an empty string and split sentence by spaces
         String[] arrWithoutPunctuation = strSentence.replaceAll("[.,;:?!'@#*()]", "").split("\\s+");
 
         System.out.print("\nChoose sorting order [ASC or DES]: ");
         String strSortingOrder = input.nextLine();
 
+        extracted(arrWithoutPunctuation, strSortingOrder);
+    }
+
+    private static void extracted(String[] arrWithoutPunctuation, String strSortingOrder) {
         if (strSortingOrder.equalsIgnoreCase("ASC")) { // accepts ESC, esc, Esc,.. input
             for (int i = 1; i < arrWithoutPunctuation.length; i++) {
                 String strTemp = arrWithoutPunctuation[i];
                 int intCounterAsc = i - 1;
                 while (intCounterAsc >= 0 && strTemp.length() < arrWithoutPunctuation[intCounterAsc].length()) {
-                    arrWithoutPunctuation[intCounterAsc + 1] = arrWithoutPunctuation[intCounterAsc]; // shift element to the right
+                    arrWithoutPunctuation[intCounterAsc + 1] = arrWithoutPunctuation[intCounterAsc]; // shift element to
+                                                                                                     // the right
                     intCounterAsc--;
                 }
                 arrWithoutPunctuation[intCounterAsc + 1] = strTemp; // insert temp string to ist correct position
             }
             System.out.print("Sentence in ascending order: ");
-            for (int i = 0; i < arrWithoutPunctuation.length; i++) {  // print in ascending order
-                    System.out.printf(" %s ", arrWithoutPunctuation[i]);
-                }
+            for (int i = 0; i < arrWithoutPunctuation.length; i++) { // print in ascending order
+                System.out.printf(" %s ", arrWithoutPunctuation[i]);
             }
-        else if (strSortingOrder.equalsIgnoreCase("DES")) {
+        } else if (strSortingOrder.equalsIgnoreCase("DES")) {
             for (int i = 1; i < arrWithoutPunctuation.length; i++) {
                 String strTemp = arrWithoutPunctuation[i];
                 int intCounterDes = i - 1;
                 while (intCounterDes >= 0 && strTemp.length() > arrWithoutPunctuation[intCounterDes].length()) {
-                    arrWithoutPunctuation[intCounterDes + 1] = arrWithoutPunctuation[intCounterDes]; // shift element to the right
+                    arrWithoutPunctuation[intCounterDes + 1] = arrWithoutPunctuation[intCounterDes]; // shift element to
+                                                                                                     // the right
                     intCounterDes--;
                 }
                 arrWithoutPunctuation[intCounterDes + 1] = strTemp; // insert temp string to ist correct position
             }
 
             System.out.print("Sentence in descending order: ");
-            for (int i = 0; i < arrWithoutPunctuation.length; i++) { //print in descending order
+            for (int i = 0; i < arrWithoutPunctuation.length; i++) { // print in descending order
                 System.out.printf(" %s ", arrWithoutPunctuation[i]);
             }
         }
     }
 }
-    
-
